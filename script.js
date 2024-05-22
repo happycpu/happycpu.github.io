df4901f601af22c9b30c8a2e5c8eacb21ff01e3a
happycpu <happycpu@gmail.com>
2024-05-22 16:04:56 +0900

    zero padding

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ async function encryptText() {
 
     const iv = new Uint8Array([0x65, 0x66, 0x71, 0x64, 0x61, 0x73, 0x64, 0x66, 0x33, 0x34, 0x7A, 0x78, 0x65, 0x72, 0x66, 0x65]);
     
-    const inputText = document.getElementById('inputText').value;
+    let inputText = document.getElementById('inputText').value;
+    inputText = pad(inputText);
+
     const data = new TextEncoder().encode(inputText);
 
     const cryptoKey = await window.crypto.subtle.importKey(
@@ -25,4 +27,13 @@ async function encryptText() {
     const base64String = btoa(String.fromCharCode(...encryptedArray));
     
     document.getElementById('outputText').value = base64String;
+}
+
+function pad(input) {
+    let padded = input;
+    const paddedLength = 16 - (input.length % 16);
+    if (paddedLength !== 0) {
+        padded += String.fromCharCode(0).repeat(paddedLength);
+    }
+    return padded;
 }
